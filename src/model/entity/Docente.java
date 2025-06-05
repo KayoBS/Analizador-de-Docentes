7f6117faedd243d3e23233f8388b87c5f5dfb97a
KayoBS <kayobs@hotmail.com>
2025-06-05 16:06:47 -0300

    Adição de getters e setters da classe Docente

--- src/model/entity/Docente.java
+++ src/model/entity/Docente.java
@@ -9,4 +9,56 @@ public class Docente
   private String classe_funcional;
   private String lotacao;
   private LocalDate admissao;
+
+  //GETTERS
+  public String getSiape()
+  {
+    return this.siape;
+  }
+
+  public String getNome()
+  {
+    return this.nome;
+  }
+
+  public String getClasse_funcional()
+  {
+    return this.classe_funcional;
+  }
+
+  public String getLotacao()
+  {
+    return this.lotacao;
+  }
+
+  public LocalDate getAdmissao()
+  {
+    return this.admissao;
+  }
+
+  //SETTERS
+  public void setSiape(String siape)
+  {
+    this.siape = siape;
+  }
+
+  public void setNome(String nome)
+  {
+    this.nome = nome;
+  }
+
+  public void setClasse_funcional(String classe_funcional)
+  {
+    this.classe_funcional = classe_funcional;
+  }
+
+  public void setLotacao(String lotacao)
+  {
+    this.lotacao = lotacao;
+  }
+
+  public void setAdmissao(LocalDate admissao)
+  {
+    this.admissao = admissao;
+  }
 }
